19326fc287105e270aaedae1418a87ecd3770037
Moonyll <s.pouwel@laposte.net>
2018-12-18 13:41:38 +0100

    Cex1

---
+++ Cex1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Bonjour, est-ce qu'il pleut aujourd'hui ? (indiquez true ou false)");
+         bool rainy = bool.Parse(Console.ReadLine());
+            if (rainy)       
+                Console.WriteLine("« Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie. »");
+            else
+                Console.WriteLine("« Bonjour, il ne pleut pas aujourd’hui, bonne journée. »");
+        }
+    }
+      
+    
+    
+}
