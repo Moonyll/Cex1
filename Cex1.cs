d2664e97d8e9792ae5448d9ecbf03052ff68d582
Moonyll <45941071+Moonyll@users.noreply.github.com>
2018-12-19 10:56:21 +0100

    Update Cex1.cs

--- Cex1.cs
+++ Cex1.cs
@@ -22,3 +22,20 @@ namespace ConsoleApp29
     
     
 }
+/* CORRIGE :
+class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Bonjour, est-ce qu'il pleut aujourd'hui ? (indiquez true ou false)");
+            bool rainy = bool.Parse(Console.ReadLine());*/
+            bool rainy = true; 
+            if (rainy) // if (!rainy)        ! négation 
+                // if (rainy == false)       == op. de comparaison  = attribut de valeur
+            {
+                Console.WriteLine("« Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie. »"); // \ pour les guillemets
+            }
+            else
+            {
+                Console.WriteLine("« Bonjour, il ne pleut pas aujourd’hui, bonne journée. »"); */
+            }
